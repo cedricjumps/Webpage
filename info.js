dbb63d2dac5f515d35d2b83f30aec0ad45b36b37
cedricjumps <35538990+cedricjumps@users.noreply.github.com>
2018-04-18 21:13:37 -0500

    Create info.js

---
+++ info.js
@@ -0,0 +1,5 @@
+var name= "Cedric";
+
+document.write ("span id = "footer"> Jacob's Website </span> <br/>");
+document.write (" <p>© 2018 "+name+" <br/>");
+document.write ("Retrieved on "+Date()+"</p>");
